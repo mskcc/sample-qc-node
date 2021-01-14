ce8140422b2766e8653cab9d30f1e95581e26721
anna patruno <apatruno618@gmail.com>
2021-01-14 16:48:27 -0500

    Investigator decision, loader
    - Investigator decision set to true if user is not a lab member
    - corresponding values display in handsontable
    - loading overlay displayed while waiting for report data, flag is set in vuex store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,7 @@
     <!-- <HelloWorld /> -->
     <span>
       <show-header></show-header>
+      <loading :active.sync="isLoading" :can-cancel="true" :on-cancel="onCancel" :is-full-page="fullPage"></loading>
       <router-view></router-view>
     </span>
   </div>
@@ -12,12 +13,22 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue';
 import ShowHeader from './components/ShowHeader.vue';
+// Import component
+import Loading from 'vue-loading-overlay';
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
     ShowHeader,
+    Loading,
+  },
+  computed: {
+    isLoading: function() {
+      return this.$store.getters.getIsLoading;
+    },
   },
 };
 </script>
